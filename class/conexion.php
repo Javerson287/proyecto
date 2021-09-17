916dc8dbae38cf86f59b2261440ca80668d67455
Javerson287 <81368864+Javerson287@users.noreply.github.com>
2021-09-16 19:07:04 -0500

    Add files via upload

---
+++ class/conexion.php
@@ -0,0 +1,13 @@
+<?php
+
+ class Conex
+
+ {
+	 static function conectar()
+ { 
+	 return mysqli_connect('localhost', 'id17297349_tipkey', '@-0HUUfZ|%lz{GLF', 'id17297349_tipke_sena');
+ } 
+
+ } 
+
+?>
